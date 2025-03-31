c1b69220879ec359547e7d8192c4baf28bdaf86a
tomaszpakula <tomasz.pakula@gmail.com>
2025-03-31 13:15:09 +0200

    wymagania na 5

--- zadanie2/skrypt.sh
+++ zadanie2/skrypt.sh
@@ -1,4 +1,4 @@
-sbt run &
-sleep 10
-ngrok http 9000 &
-wait  
+#!/bin/bash
+docker build -t tomaszpakula/zadanie2:latest .
+docker run -d --name zadanie2 tomaszpakula/zadanie2:latest
+docker exec -d zadanie2 ngrok http 9000
